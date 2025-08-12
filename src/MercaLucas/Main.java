b964c75a08eb97b2286d0a6738d28d546377219c
Lucas Boaratti <lucasboaratti15@gmail.com>
2025-08-12 12:58:06 -0300

    Finalizando o mercadinho².

--- src/MercaLucas/Main.java
+++ src/MercaLucas/Main.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Carrinho carrinho;
-
         Scanner input = new Scanner(System.in);
 
+        //Mensagem de boas vindas
         System.out.println("Olá, seja bem vindo(a) ao MercaLucas! Para começar, digite suas informações para entrar no Mercadinho :D");
 
+        //Cadastro do cliente, com as letras convertidas para minúsculas
         System.out.println("Digite seu nome: ");
         String nome = input.nextLine().toLowerCase();
 
         System.out.println("Digite seu email: ");
         String email = input.nextLine().toLowerCase();
 
+        //Validações do cadastro
         if (nome.isEmpty() && email.isEmpty()) {
             System.out.println("Digite seu nome e sua senha, por favor.");
             return;
@@ -39,10 +40,12 @@ public class Main {
 
         System.out.println("\nAgora, você irá escolher três produtos para comprar:\n");
 
+        //Criando 3 produtos fixos para o mercado
         Produtos produto1 = new Produtos("Chocolate", 9.99);
         Produtos produto2 = new Produtos("Doritos", 15.99);
         Produtos produto3 = new Produtos("Laranja", 19.99);
 
+        //Exibindo os produtos para o usuário
         produto1.exibirInfo();
         produto2.exibirInfo();
         produto3.exibirInfo();
@@ -51,6 +54,7 @@ public class Main {
         double[] listaPrecos = new double[3];
         int produtosAdicionados = 0;
 
+        //Laço for para percorrer os produtos escolhidos pelo usuário
         for (int i = 0; i < 3; i++) {
             System.out.println("\nDigite o nome do produto: ");
             listaProdutos[i] = input.nextLine().toLowerCase();
@@ -59,6 +63,7 @@ public class Main {
             listaPrecos[i] = input.nextDouble();
             input.nextLine();
 
+            //Validações dos produtos e seus preços
             if (listaProdutos[i].equals("chocolate") && listaPrecos[i] == 9.99) {
                 System.out.println("Produto e preço adicionado.\n");
             }
@@ -79,8 +84,10 @@ public class Main {
             System.out.println("Você deseja continuar comprando os produtos? (digite sim ou não): ");
             String escolha = input.nextLine().toLowerCase();
 
+            //Verificando se o usuário não quer continuar comprando
             if (escolha.equals("nao") || escolha.equals("n") || escolha.equals("não")) {
                 System.out.println("Produtos adicionados: ");
+                //Exibindo os produtos comprados
                 for (int j = 0; j < produtosAdicionados; j++) {
                     System.out.printf("- " + listaProdutos[j] + ": R$ " + listaPrecos[j] + "\n");
                 }
@@ -89,7 +96,9 @@ public class Main {
                 System.out.println("Digite o nome do produto para aplicar o desconto: ");
                 String desconto = input.nextLine().toLowerCase().trim();
 
+                //Laço for para percorrer o item escolhido pelo usuário para aplicar o desconto
                 for (int k = 0; k < listaProdutos.length; k++) {
+                    //Aplicando o desconto
                     if (listaProdutos[k].equals(desconto)) {
                         double valorDesconto = listaPrecos[k] * 0.4;
                         double valorTotal = listaPrecos[k] - valorDesconto;
